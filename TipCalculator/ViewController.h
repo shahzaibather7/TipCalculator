216f79559d33e1ae9bb261b600c7214e16d9da3e
Shahzaib Ather <shahzaibather7@github.ca>
2017-08-04 11:27:15 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Mohammad Shahzaib Ather on 2017-08-04.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Mohammad Shahzaib Ather on 2017-08-04.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
